4421e419149b9b999622cf20159c3ba586711f4b
grace <grace@gmail.com>
2022-07-25 05:34:09 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * _print_rev_recursion - Prints string followed by new line
@@ -10,6 +10,6 @@ void _print_rev_recursion(char *s)
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-		putchar(*s)
+		putchar(*s);
 	}
 }
